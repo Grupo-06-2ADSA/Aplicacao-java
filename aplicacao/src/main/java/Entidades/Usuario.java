f1575707c9d450638a58e8cbe88c9f4ff6e3d72c
HeloSalgado <heloisa.salgado@sptech.school>
2024-05-29 13:02:20 -0300

    Mudança no scanner

--- aplicacao/src/main/java/Entidades/Usuario.java
+++ aplicacao/src/main/java/Entidades/Usuario.java
@@ -20,42 +20,47 @@ public class Usuario {
     }
 
     public static void FazerLogin() throws IOException, InterruptedException {
-        Scanner leitor = new Scanner(System.in);
-
-        System.out.println("+-------------------------------------------+");
-        System.out.println("|            Entre na sua conta              ");
-        System.out.println("|   Pressione a tecla [ENTER] para começar   ");
-
-        if (leitor.hasNextLine()) {
-            System.out.print("| Email: ");
-            String email = leitor.next();
-
-            System.out.print("| Senha: ");
-            String senha = leitor.next();
+        try {
+            Scanner leitor = new Scanner(System.in);
+            Scanner enter = new Scanner(System.in);
 
             System.out.println("+-------------------------------------------+");
+            System.out.println("|            Entre na sua conta              ");
+            System.out.println("|   Pressione a tecla [ENTER] para começar   ");
 
-            Usuario usuario = new Usuario(email, senha);
+            if (enter.hasNextLine()) {
+                System.out.print("| Email: ");
+                String email = leitor.nextLine();
 
-            boolean usuarioExiste = UsuarioDAO.verificarUsuario(usuario);
+                System.out.print("| Senha: ");
+                String senha = leitor.nextLine();
 
-            if (usuarioExiste) {
-                App.CapturarDados();
+                System.out.println("+-------------------------------------------+");
+
+                Usuario usuario = new Usuario(email, senha);
+
+                boolean usuarioExiste = UsuarioDAO.verificarUsuario(usuario);
+
+                if (usuarioExiste) {
+                    App.CapturarDados();
 //                    Looca looca = new Looca();
 //                    Rede rede = looca.getRede();
 //
 //                    JSONObject json = new JSONObject();
 //                    json.put("text", "O usuário " + Usuario.getEmail() + " Realizou login na máquina: " + rede.getParametros().getHostName());
 //                    Slack.sendMessage(json);
+                } else {
+                    System.out.println("Dados incorretos, tente novamente.");
+                    FazerLogin(); // Chama o método fazerLogin novamente após uma tentativa malsucedida
+                }
             } else {
-                System.out.println("Dados incorretos, tente novamente.");
-                FazerLogin(); // Chama o método fazerLogin novamente após uma tentativa malsucedida
+                System.out.println("Leitor não disponível.");
             }
-        } else {
-            System.out.println("Leitor não disponível.");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler entrada do usuário: " + e.getMessage());
         }
-
     }
+
 
     public static String getEmail() {
         return email;
