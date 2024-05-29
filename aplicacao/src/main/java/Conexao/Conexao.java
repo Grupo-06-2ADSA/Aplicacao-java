622fe79ef58068d4ef51cc67f4144f65a7026a24
HeloSalgado <heloisa.salgado@sptech.school>
2024-05-29 18:39:09 -0300

    Mudança do host

--- aplicacao/src/main/java/Conexao/Conexao.java
+++ aplicacao/src/main/java/Conexao/Conexao.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-        private static final String url = "jdbc:mysql://localhost:3306/MindCore";
+        private static final String url = "jdbc:mysql://mysql:3306/MindCore";
         // jdbc:mysql://localhost:porta/bancodedados
         private static final String user = "mindcore"; // nome do seu usuário
         private static final String password = "mindcore123grupo6"; // sua senha
